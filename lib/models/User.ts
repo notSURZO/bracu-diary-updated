cd045603a466c467df0e6de78e541acc59b71153
Surzo <137491427+notSURZO@users.noreply.github.com>
2025-08-10 00:07:46 +0600

    Search functionality implemented

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -1,15 +1,19 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-interface IUser extends Document {
+export interface IUser extends Document {
   name: string;
   username: string;
   email: string;
+  student_ID: string;
+  picture_url: string;
 }
 
 const UserSchema: Schema = new Schema({
   name: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
+  student_ID: { type: String, required: true, unique: true },
+  picture_url: { type: String, default: '' },
 });
 
 export default mongoose.models.User || mongoose.model<IUser>('User', UserSchema);
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['example.com', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'res.cloudinary.com'],
+  },
 };
 
 export default nextConfig;
